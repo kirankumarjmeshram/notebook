9573062b39ef2bf78d158ca80dd3d2aaa7d0198b
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-06-24 23:14:27 +0530

    setup server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config();
 app.get("/", (req,res) => {
     res.send("<h1>Hello World<h1>")
 })
-app.listen(3000,console.log("Server is running on port 3000"));
+
+app.get("/api/notes",(req,res)=>{
+    res.send(notes)
+})
+
+app.get("/api/notes/:id",(req,res)=>{
+    const note = notes.find((el)=>el._id == req.params.id)
+    res.send(note)
+})
+
+const PORT = process.env.PORT || 3000 ;
+app.listen(PORT,console.log(`Server is running on port ${PORT}`));
 
